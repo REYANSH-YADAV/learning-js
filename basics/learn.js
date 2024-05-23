d6d964d4fb0664a7c975c7831f7fc06562195f75
REYANSH YADAV <143245387+REYANSH-YADAV@users.noreply.github.com>
2024-05-23 12:43:43 +0000

    configuration change

--- basics/learn.js
+++ basics/learn.js
@@ -1,1 +1,107 @@
-console.log("come on")
+// console.log("come on");
+// "use strict"  //treat all js code as newer version
+
+// console.log("my name is reyansh");
+// let marks =20;
+// marks="babbar";
+
+// console.log(marks);
+// console.log(typeof(marks));
+
+
+// let num1=56;
+// let num2=64;
+// let mul=num1*num2;
+
+// console.log(mul);
+// console.log(num2%num1);
+
+// let number1 = 10;
+// let number2 = 20;
+
+// let sum = number1 + number2;
+
+// console.log(`The sum of ${number1} + ${number2} is equal to ${sum}`);
+
+// Output : The sum of 10 + 20 is equal to 30
+
+// console.log('the mul of ${num1} * ${num2} is equal to ${mul}');
+
+
+// let string1="aur bakchodi karo";
+// let string2="bs bs bhai jada ho gaya";
+// let multilineString=`${string1},${string2}`;
+// console.log(multilineString);
+
+
+// let person={
+//     name:'reyansh',
+//     age:19,
+//     message:function(){
+//         console.log("hello,my name is "+this.name);
+//     }
+// };
+// person.country="INDIA";
+
+// person.message();
+// console.log(person);
+
+// console.log(person.name);
+
+
+// function sum(x,y){
+//     let res=x+y;
+//     return res;
+// }
+// let outt=sum(3,4);
+// console.log(outt);
+
+// setTimeout(function() {
+//     console.log('This is an anonymous function called after 1 second.');
+//   }, 1000);
+
+
+//   const numbers=[-1,100,25,-50,45];
+//   const positivnumbers=numbers.filter(function(numbers){
+//     return numbers>0;
+//   });
+
+//   console.log(positivnumbers);
+
+//   let obj={
+//     name:"reyansh",
+//     "full name":"reyansh yadav",
+//     age:19,
+//     wight:76,
+//     greet: function(){
+//       console.log("hello jee kya kr rhe ho!");
+//     }
+//   };
+//   console.log(obj);
+//   obj.greet();
+
+//   console.log(typeof(obj));
+
+
+//   let arr=[1,2,3,4,5];
+//   let ansarr=arr.map((Number)=>{
+//     return Number*Number;
+//   }) 
+//   console.log(ansarr);
+  
+//   class Human{
+//     age=13;
+//     wt=76;
+//     //behaviour
+//     walking(){
+//       console.log("i am walking")
+//     }
+//   }
+//   const  reyansh=new Human(); 
+//   console.log(reyansh.walking());
+
+// let mydate=new Date();
+// console.log(mydate);
+
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
