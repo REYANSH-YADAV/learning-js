840a5de6cdaaefbdaa9dcbbb9a9da8c72659b976
REYANSH YADAV <143245387+REYANSH-YADAV@users.noreply.github.com>
2024-05-28 06:45:08 +0000

    config change

--- basics/learn.js
+++ basics/learn.js
@@ -388,4 +388,27 @@
 //     console.log(`${key} shortcut is for ${myObject[key]}`);
 // }
 
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+// const newNums = myNums.filter( (num) => {
+//     return num > 4;
+// } )
+// console.log(newNums);
+
+// const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+// console.log(newNums);
+// const newNum = myNumers
+//                 .map((num) => num * 10 )
+//                 .map( (num) => num + 1)
+//                 .filter( (num) => num >= 40)
+
+// console.log(newNum);
+
+// const mynums=[1,2,3];//reduce
+// let acc=0;
+// const mytotal=mynums.reduce(function(acc,currval){
+//     return acc + currval;
+// })
+// console.log(mytotal);
